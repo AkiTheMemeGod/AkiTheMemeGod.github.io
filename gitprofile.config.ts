a7d82c6ee4329084b31e7318aa430e97a926579a
Akash K <142716100+AkiTheMemeGod@users.noreply.github.com>
2025-03-03 22:52:04 +0530

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -20,7 +20,7 @@ const CONFIG = {
         limit: 20, // How many projects to display.
         exclude: {
           forks: false, // Forked projects will not be displayed if set to true.
-          projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
+          projects: ['AkiTheMemeGod/AkiTheMemeGod.github.io', 'AkiTheMemeGod/AkiTheMemeGod'], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
         },
       },
       manual: {
